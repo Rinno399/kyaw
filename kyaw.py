858581a27274b84d5caf56b7c2f11d1361881819
Rinno399 <78363610+Rinno399@users.noreply.github.com>
2023-04-16 11:00:35 +0630

    Add files via upload

--- kyaw.py
+++ kyaw.py
@@ -2,7 +2,21 @@ import requests
 import random
 import time
 from bs4 import BeautifulSoup
-def get_token():
+
+def uag():
+    aa='Mozilla/5.0 (Linux; Android'
+    ver = random.choice(['1','2','3','4','5','6','7','8','9','10','11','12'])
+    b=random.choice(['6','7','8','9','10','11','12','13'])
+    c=f'Readmi Note;{ver}'
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    useragent=f'{aa} {b}; {c}) {g}{h}.{i}.{j}.{k} {l}'
+    return useragent
+def get_token(uag):
     res1=requests.get("https://kyayzuutaw.com/blog/types-of-artificial-intelligence-know-the-main-ones?token=q2e9u4sgrgsS544qw3039&alias=w2t7x",allow_redirects=False)
 
     cookies=(res1.cookies.get_dict())
@@ -10,7 +24,7 @@ def get_token():
     
     
     headers1={"Cookie": cookie,
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36",
+    "User-Agent": uag,
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9"}
@@ -25,17 +39,7 @@ def get_token():
     t_oken.append(token[0])
     t_oken.append(cookie2)
     return t_oken
-token=get_token()
 
-headers2={"Cookie": token[1],
-"Content-Length": "93",
-"Origin": "https://kyayzuutaw.com",
-"Content-Type": "application/x-www-form-urlencoded",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36",
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"Referer": "https://kyayzuutaw.com/blog/how-to-make-money-as-a-freelancer-check-out-our-tips",
-"Accept-Encoding": "gzip, deflate",
-"Accept-Language": "en-US,en;q=0.9"}
 
 
 def url_loop(alias,token,headers,token2):
@@ -57,18 +61,34 @@ while True:
 print(alias)
 timing=input("Enter loop times::")
 delay=input("Enter Delay Time[ex,0.1,0.002:")
+delay2=input("Enter Delay2 Time[ex,0.1,0.002:")
 if delay == "":
     delay=0
 
 for l_di in alias:
+    
     print(l_di)
     for i in range(1,int(timing)+1):
+        userag=uag()
+        token=get_token(userag)
+
+        headers2={"Cookie": token[1],
+        "Content-Length": "93",
+        "Origin": "https://kyayzuutaw.com",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": userag,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Referer": "https://kyayzuutaw.com/blog/how-to-make-money-as-a-freelancer-check-out-our-tips",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.9"}
         if url_loop(l_di,f"ags{random.randrange(111,999999)}bg{random.randrange(1,999)}y",headers2,token) == 302:
             print(f"[{l_di}+{i}+--]OK>>",end="\r")
         else:
             print("URL LAST Code Not found")
-        time.sleep(int(delay))
+        time.sleep(5)
     print("DONE for "+l_di)
+    time.sleep(int(delay))
+    
 
 
 
